659cba5a671a6da3e3e786fd3fc74e2e4532be97
Atru8729 <a.truskauskas1@gmail.com>
2021-10-28 20:07:37 +0300

    components, pipes, services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ComponentsComponent } from './components/components.component';
+import { PipesComponent } from './app/pipes/pipes.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsComponent,
+    PipesComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule
